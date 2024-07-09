086ff10e41c02da396f0b37382a7bda4d6def3f7
Francesco Caglianone <francescocaglianone@gmail.com>
2024-07-09 10:29:52 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+// Typing animation in Index.html
 document.addEventListener("DOMContentLoaded", function() {
     const nameElement = document.getElementById("name");
     const text = "Francesco Caglianone";
@@ -17,38 +20,3 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
-
-
-// WRITE IN LOOP
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const nameElement = document.getElementById("name");
-//     const text = "Francesco Caglianone";
-//     let index = 0;
-//     let isDeleting = false;
-
-//     function type() {
-//         if (!isDeleting) {
-//             if (index < text.length) {
-//                 nameElement.textContent += text.charAt(index);
-//                 index++;
-//                 setTimeout(type, 200);
-//             } else {
-//                 isDeleting = true;
-//                 setTimeout(type, 3000); // Pause before deleting
-//             }
-//         } else {
-//             if (index > 0) {
-//                 nameElement.textContent = text.substring(0, index - 1);
-//                 index--;
-//                 setTimeout(type, 50);
-//             } else {
-//                 isDeleting = false;
-//                 setTimeout(type, 500); // Pause before typing again
-//             }
-//         }
-//     }
-
-//     type();
-// });
